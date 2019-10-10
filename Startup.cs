f461a793074eb0a40cfab7b7972ea09754054460
Jethro Tuburan <tuburanjethro@gmail.com>
2019-10-11 12:09:15 +1300

    Configuring the request processing pipeline

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement
 {
@@ -25,8 +26,9 @@ namespace EmployeeManagement
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the 'HTTP request pipeline'.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        // Configure method gets called by the runtime. Use this method to configure the 
+        // 'HTTP request pipeline'.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
@@ -34,24 +36,46 @@ namespace EmployeeManagement
                 app.UseDeveloperExceptionPage();
             }
 
-            //Middleware: IapplicationBUilder.Run()
-            app.Run(async (context) =>
+            // Use() adds a middleware delagate defined in-line to the applications request pipeline
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW1: Incoming Request");
+                await next();
+                logger.LogInformation("MW1: Outgoing response");
+            });
+
+            // Middleware: IapplicationBUilder.Run() runs the callback function
+            // If it is the only thing in the pipeline, all requests will be handled by this single middleware and its only response will be _config["MyKey"]
+            // Therefore no matter what the URL is it will respond with _config["MyKey"]. 
+            // Even if you want to serve a file from wwwroot, it won't show up.
+            app.Use(async (context, next) =>
             {
                 //Examples
                 {
-                //await context.Response.WriteAsync("Hello World!");
-                //name of the process that is running and hosting our code (IIS or dotnet(Kestral))
-                //await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                    //await context.Response.WriteAsync("Hello World!");
+                    //name of the process that is running and hosting our code (IIS or dotnet(Kestral))
+                    //await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+
+                    //await context.Response.WriteAsync(_config["MyKey"]);
                 }
 
-                await context.Response.WriteAsync(_config["MyKey"]);
+                logger.LogInformation("MW2: Incoming Request");
+                await next();
+                logger.LogInformation("MW2: Outgoing response");
+            });
+
+            //This second middleware will never be hit because of .Run() is a terminal middleware. 
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("MW3: Request handled and response produced");
+                logger.LogInformation("MW3: Request handled and response produced");
             });
         }
     }
 }
 
-// MiddleWare in ASP.NET Core
-/* - Has access to both Request and Response
+/* MiddleWare in ASP.NET Core:
+ * - Has access to both Request and Response
  * - May simply pass the Request to the next Middleware
  * - May handle the Request and short-circuit the pipeline
  * - May process the outgoing Response
@@ -59,4 +83,9 @@ namespace EmployeeManagement
  * 
  * Example Pipeline:
  *     ... <----> Logging <----> Static Files <----> MVC <----> ...
+ */
+
+/* Configure Request Processing Pipeline:
+ *  Everything that happens 'before the next() method' is invoked in each of the middleware components, 
+ *  happens as the 'REQUEST' travels from middleware to middleware through the pipeline.
  */
